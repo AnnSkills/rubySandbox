6df7722fedd2aaa208e3c444574137c44cf2d275
Anna <khomiakova25@gmail.com>
2022-09-09 16:27:35 +0300

    edit def case

--- tasks1-10/task6.rb
+++ tasks1-10/task6.rb
@@ -1,16 +1,17 @@
 class Calculation
-  def sumOfSquares(natural)
+  def sum_of_squares(natural)
     (1...natural).sum{ |x| x**2}
   end
 
-  def squareOfSum(n)
+  def square_of_sum(n)
     (1...n).sum.pow(2)
   end
 
   def difference(n)
-    squareOfSum(n) - sumOfSquares(n)
+    square_of_sum(n) - sum_of_squares(n)
   end
 end
+
 
 
 
