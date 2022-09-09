85ddf5f02d47b741d183c2e2045e406d492ff0fc
Anna <khomiakova25@gmail.com>
2022-09-09 18:15:55 +0300

    task 10 commit

---
+++ tasks1-10/task10.rb
@@ -0,0 +1,17 @@
+require 'prime'
+
+def is_prime?(number)
+  Prime.prime?(number)
+end
+
+def prime_sum_2mln
+  finish = 2_000_000
+  sum = 0
+  (0...finish).each do |count|
+    if is_prime?(count)
+      sum += count
+    end
+  end
+  sum
+end
+p prime_sum_2mln
