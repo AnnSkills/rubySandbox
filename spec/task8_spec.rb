b23eaf3c823495420f0b42108a4a521065857a75
Anna <khomiakova25@gmail.com>
2022-09-09 18:07:37 +0300

    task 10

---
+++ spec/task8_spec.rb
@@ -0,0 +1,8 @@
+require 'task8'
+
+describe 'Task8' do
+  it 'Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?' do
+    foundation = Task8.new
+    expect(foundation)
+  end
+end
