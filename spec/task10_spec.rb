5013dbf0a652e0afa1364d98125f6aa4650b8bfb
Anna <khomiakova25@gmail.com>
2022-09-09 18:08:49 +0300

    Revert "task 10"
    This reverts commit b23eaf3c823495420f0b42108a4a521065857a75.

--- spec/task10_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'task10'
-
-describe 'Task10' do
-  it 'Find the sum of all the primes below two million' do
-    foundation = prime_sum_2mln
-    expect(foundation.eql?(142913828922))
-  end
-end
--- spec/task8_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'task8'
-
-describe 'Task8' do
-  it 'Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?' do
-    foundation = Task8.new
-    expect(foundation)
-  end
-end
