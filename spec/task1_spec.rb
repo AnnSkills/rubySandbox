97b45a886fcae7884918d25841626a98b90f1de2
Anna <khomiakova25@gmail.com>
2022-09-09 13:59:15 +0300

    refactor(main): remove unused line, remove unused return, rename vars

--- spec/task1_spec.rb
+++ spec/task1_spec.rb
@@ -1,6 +1,7 @@
-require '/home/hanna.khamiakova/RubymineProjects/sandbox/tasks1-10/task1'
+require 'task1'
+
 describe 'Task1' do
-  it "Find the sum of all the multiples of 3 or 5 below 1000." do
+  it 'Find the sum of all the multiples of 3 or 5 below 1000' do
     calculator = Task1.new
     expect(calculator.sum_of_divisors).to eq(233168)
   end
--- tasks1-10/task1.rb
+++ tasks1-10/task1.rb
@@ -5,26 +5,21 @@ class Task1
     count = 1
     boundary = 1000
     until count == boundary
-         unless count.is_a?(Integer)
-           puts("Only natural nums")
-         end
-         if if_divisor(count).eql?(true)
-          sum+=count
-         end
-         count += 1
+      unless count.is_a?(Integer)
+        puts("Only natural nums")
+      end
+      if is_divisor?(count)
+        sum += count
+      end
+      count += 1
     end
-    return sum
+    sum
   end
 
   #check if it's divisor
-  def if_divisor(a)
-    if a % 3 == 0 || a % 5 == 0
-      return true
-    end
-    return false
+  def is_divisor?(dividend)
+    dividend % 3 == 0 || dividend % 5 == 0
   end
-
 end
-
 ca = Task1.new
 p ca.sum_of_divisors()
