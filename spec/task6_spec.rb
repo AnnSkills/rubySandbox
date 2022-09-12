0980798b1ccb8d67fb7e4ec07af15409926a21f2
Anna <khomiakova25@gmail.com>
2022-09-12 13:50:03 +0300

    (tasks 6) Fix: -  spaces in block, indentation in def-end

--- spec/task6_spec.rb
+++ spec/task6_spec.rb
@@ -2,7 +2,7 @@ require 'task6'
 
 describe 'Calculation' do
   it 'Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.' do
-  calc = Calculation.new
-  expect(calc.difference(100).eql?(24174150))
+    calc = Calculation.new
+    expect(calc.difference(100).eql?(24174150))
   end
 end
