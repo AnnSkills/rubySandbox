85381892e4b47952cfcfe4ec988115650c02a291
Ann <khomiakova25@gmail.com>
2022-09-13 18:40:18 +0300

    (task 9) Fixed: add constants, replace break vars to break

--- tasks1-10/task9.rb
+++ tasks1-10/task9.rb
@@ -1,5 +1,8 @@
 class Triplet
   FIXNUM_MAX = (2**(0.size * 8 -2) -1)
+  TRIPLE_SUM = 1000
+  MAX_VAL = 1000
+  MIN_VAL = 3
   attr_accessor :a, :b, :c
   def initialize(a, b, c)
     @a = a
@@ -17,21 +20,20 @@ class Triplet
 
   def self.find_triplet
     product = ""
-    max = 1000
-    min = 3
     sum = nil
-    count = "start"
-    while count != "stop"
-     combinations = (min..max).to_a.combination(3)
+    count = 0
+    while count == 0
+     combinations = (MIN_VAL..MAX_VAL).to_a.combination(3)
      combinations.each_with_object([]) do |combination, results|
        triplet = Triplet.new(*combination)
        if triplet.is_pythagorean_triplet?
-         if triplet.triplet_sum == 1000
+         if triplet.triplet_sum == TRIPLE_SUM
            results << triplet if sum.nil? || triplet.triplet_sum == sum
            product = triplet.a.to_s + triplet.b.to_s + triplet.c.to_s
-           count = "stop"
+           break
            product
          end
+         break
        end
      end
     end
